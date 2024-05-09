9ad6e2f46b9ee1127454d11ca8b572a519f90249
solovyovsdom <joelborofsky@mac.com>
2024-05-09 14:32:51 -0500

    refactor: Update theme toggler class name and variable names

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 import './styles/style.scss'
 
+// Theme toggle
+document.addEventListener('DOMContentLoaded', () => {
+  const lightbtn = document.getElementById('light-btn')
+  const darkBtn = document.getElementById('dark-btn')
+
+  function setTheme(theme) {
+    document.documentElement.setAttribute('data-theme', theme)
+
+    if (theme === 'dark') {
+      darkBtn.classList.add('active')
+      lightbtn.classList.remove('active')
+    } else {
+      lightbtn.classList.add('active')
+      darkBtn.classList.remove('active')
+    }
+  }
+
+  lightbtn.addEventListener('click', () => {
+    setTheme('light')
+  })
+  darkBtn.addEventListener('click', () => {
+    setTheme('dark')
+  })
+
+  const currentTheme = document.documentElement.getAttribute('data-theme') || 'light'
+  setTheme(currentTheme)
+})
