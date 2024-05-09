d8f6244c0b49558ff2dfdff4e1c0c36467beaf40
solovyovsdom <joelborofsky@mac.com>
2024-05-09 14:42:45 -0500

    refactor: Improve theme toggler implementation and transition

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import './styles/style.scss'
 
-// Theme toggle
 document.addEventListener('DOMContentLoaded', () => {
-  const lightbtn = document.getElementById('light-btn')
+  const lightBtn = document.getElementById('light-btn')
   const darkBtn = document.getElementById('dark-btn')
 
   function setTheme(theme) {
@@ -10,25 +9,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (theme === 'dark') {
       darkBtn.classList.add('active')
-      lightbtn.classList.remove('active')
+      lightBtn.classList.remove('active')
     } else {
-      lightbtn.classList.add('active')
+      lightBtn.classList.add('active')
       darkBtn.classList.remove('active')
     }
+    localStorage.setItem('theme', theme)
   }
 
-  lightbtn.addEventListener('click', () => {
-    setTheme('light')
-  })
-  darkBtn.addEventListener('click', () => {
-    setTheme('dark')
-  })
+  lightBtn.addEventListener('click', () => setTheme('light'))
+  darkBtn.addEventListener('click', () => setTheme('dark'))
 
-   const prefersDarkScheme = window.matchMedia(
-     '(prefers-color-scheme: dark)'
-   ).matches
-   const currentTheme =
-     document.documentElement.getAttribute('data-theme') ||
-     (prefersDarkScheme ? 'dark' : 'light')
-   setTheme(currentTheme)
+  const prefersDarkScheme = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches
+
+  const savedTheme = localStorage.getItem('theme')
+  const currentTheme = savedTheme || (prefersDarkScheme ? 'dark' : 'light')
+  setTheme(currentTheme)
 })
